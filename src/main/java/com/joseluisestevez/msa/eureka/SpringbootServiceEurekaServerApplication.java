1331e19fa464d512983739b2ab633e4aa42a09ec
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-01-28 12:25:00 +0100

    New Spring Boot project - Eureka Server

---
+++ src/main/java/com/joseluisestevez/msa/eureka/SpringbootServiceEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.joseluisestevez.msa.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServiceEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiceEurekaServerApplication.class, args);
+	}
+
+}
